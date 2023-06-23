7747113aeb55ea7a50cc1797e06bb2b7c5f276b0
Arthur-MONNET <artmon2002@gmail.com>
2023-06-23 14:54:24 +0200

    change websocket url connection

--- plugins/websocket.client.ts
+++ plugins/websocket.client.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin(() => {
     console.log(config.public)
     // Établir la connexion WebSocket avec le serveur
     const wsProtocol = window.location.protocol === "https:" ? "wss" : "wss";
-    const ws = new WebSocket(`${wsProtocol}://${config.public.WS_HOST}.eu.ngrok.io`);
+    const ws = new WebSocket(`${wsProtocol}://${config.public.WS_HOST.replace("http://", "").replace("https://", "")}:${config.public.WS_PORT}`);
     return {
         provide: {ws}
     }
